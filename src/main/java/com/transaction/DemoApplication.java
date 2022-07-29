ee9095d5da1aec224ff2b32f171b462a39a843f8
alliano <allianoanonymous@gmail.com>
2022-07-29 23:16:11 +0700

    add anotation @EnableChaching to enable caching data

--- src/main/java/com/transaction/DemoApplication.java
+++ src/main/java/com/transaction/DemoApplication.java
@@ -2,7 +2,9 @@ package com.transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching // => di main class kita anotasi ini untuk meng enable catch data dari databse
 @SpringBootApplication
 public class DemoApplication {
 
